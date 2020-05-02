8e5c8d85bd448ae318b23b861edfecc6974a7b0c
lisacopeland <lisacope@msn.com>
2020-05-02 08:21:12 -0700

    fix merge

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< HEAD
+=======
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+>>>>>>> 2affe21... Added contact form
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+<<<<<<< HEAD
   constructor() { }
+=======
+  contactForm: FormGroup;
+
+  constructor(private snackBar: MatSnackBar) { }
+>>>>>>> 2affe21... Added contact form
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+<<<<<<< HEAD
+=======
+  initForm() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl(''),
+      subject: new FormControl(''),
+      message: new FormControl('', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      this.snackBar.open('Please fill in required fields', '', {
+        duration: 2000,
+      });
+      return;
+    }
+    console.log('Form' + this.contactForm.value);
+  }
+
+>>>>>>> 2affe21... Added contact form
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,12 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
+<<<<<<< HEAD
+=======
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
+>>>>>>> 2affe21... Added contact form
 
 
 @NgModule({
   declarations: [ContactComponent],
   imports: [
     CommonModule,
+<<<<<<< HEAD
+=======
+    ReactiveFormsModule,
+    MaterialModule,
+>>>>>>> 2affe21... Added contact form
     ContactRoutingModule
   ]
 })
